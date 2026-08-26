e6871dd9f0f36ae1ce177be13ac9eade631f5c88
siikamiika <siikamiika@users.noreply.github.com>
2026-08-26 17:09:14 +0300

    fix bugs caught by pawn test

--- chess/player.py
+++ chess/player.py
@@ -1,5 +1,5 @@
 """A chess player"""
-from chess.exceptions import NotYourPiece
+from chess.exceptions import NotYourPiece, IllegalMove
 
 class Player(object):
     """The player"""
@@ -10,7 +10,9 @@ class Player(object):
     def move(self, old_position, new_position, promotion=None):
         """Check if the piece at `old_position` belongs to player and try to move it."""
         piece = self.game.board.grid[old_position].piece
-        if piece.color == self.color:
+        if piece is None:
+            raise IllegalMove(f"There is no piece at {old_position}")
+        elif piece.color == self.color:
             self.game.move(piece, new_position, promotion=promotion)
         else:
             raise NotYourPiece(f"The piece at {old_position} isn't owned by you")
